029eac6156c426a51d8b184a89c246a92f2a20cf
Raphael Braga <raffael.info@gmail.com>
2023-04-25 19:44:02 -0300

    initial comitt

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+const FirstComponent = () => {
+  return (
+    // meu comentario
+    <div>
+      <h1>Meu primeiro componente</h1>
+      <p className="teste">Meu Texto</p>
+    </div>
+  );
+};
+
+export default FirstComponent;
