e9267418f576036a0cc58b494336af4e8e29c915
Raphael Braga <raffael.info@gmail.com>
2023-04-26 11:37:58 -0300

    aula - propriedade key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';const ListRender = () => {
+import React, { useState } from 'react';
+const ListRender = () => {
   const [list] = useState(['Matheus', 'Pedro', 'Josias', 'Maria']);
+
+  const [users] = useState([
+    { id: 1, name: 'Raphael', age: 33 },
+    { id: 2, name: 'Leandro', age: 32 },
+  ]);
 
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {' '}
+            Nome: {user.name} - idade {user.age}
+          </li>
         ))}
       </ul>
     </div>
