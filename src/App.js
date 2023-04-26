0a4d7d2d5146b36ed1f424aea28defebabc49b5e
Raphael Braga <raffael.info@gmail.com>
2023-04-26 14:50:03 -0300

    aula renderização de lista em componentes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import CarDetails from './components/CarDetails';
 
 function App() {
-  //const name = 'Raphael';
+  const cars = [
+    { id: 1, brand: 'ford', color: 'azul', KM: 0 },
+    { id: 2, brand: 'vw', color: 'vermelho', KM: 234 },
+    { id: 3, brand: 'renault', color: 'verde', KM: 150000 },
+  ];
 
   return (
     <div className="App">
-      <CarDetails brand="vw" km={100000} color="azul"></CarDetails>
-      <CarDetails brand="Ford" km={0} color="Vermelha"></CarDetails>
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.KM} />
+      ))}
     </div>
   );
 }
