05cd9ae87263924c22556adf12b8318bca687d1c
Raphael Braga <raffael.info@gmail.com>
2023-04-27 10:56:20 -0300

    aula children prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 
-import CarDetails from './components/CarDetails';
+import Container from './components/Container';
 
 function App() {
-  const cars = [
-    { id: 1, brand: 'ford', color: 'azul', KM: 0 },
-    { id: 2, brand: 'vw', color: 'vermelho', KM: 234 },
-    { id: 3, brand: 'renault', color: 'verde', KM: 150000 },
-  ];
-
   return (
     <div className="App">
-      {cars.map((car) => (
-        <CarDetails brand={car.brand} color={car.color} km={car.KM} />
-      ))}
+      <Container myValue="teste">
+        <p>Este e o conteudo</p>
+      </Container>
     </div>
   );
 }
